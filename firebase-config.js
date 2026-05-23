3936a8a13a6fef03260514592bc6a4d30789cda3
CavidanK <cavidan69@gmail.com>
2026-05-24 00:10:24 +0400

    Connect db

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNzEjSD0oY3_vW34E61-fozcJ4EiQOaMo",
+    authDomain: "atb360-fef39.firebaseapp.com",
+    projectId: "atb360-fef39",
+    storageBucket: "atb360-fef39.firebasestorage.app",
+    messagingSenderId: "163618321079",
+    appId: "1:163618321079:web:292d4eb4ea73c4075bd332",
+    measurementId: "G-N0MCYYNGDK"
+};
